fc43a5e8b9a10628922aae9345b3e243436ba126
MaximeKapustaLM <maxime.kapusta@ext.leroymerlin.fr>
2020-04-27 11:17:49 +0200

    feat(auth): in mem

--- src/main/java/fr/foreach/pokego/config/CustomSecurityConfig.java
+++ src/main/java/fr/foreach/pokego/config/CustomSecurityConfig.java
@@ -1,10 +1,15 @@
 package fr.foreach.pokego.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -18,14 +23,29 @@ public class CustomSecurityConfig extends WebSecurityConfigurerAdapter {
                .antMatchers(HttpMethod.PUT, "/**").hasRole("ADMIN")
                .antMatchers(HttpMethod.DELETE, "/**").hasRole("ADMIN")
                .antMatchers(HttpMethod.GET, "/**").hasAnyRole("USER", "ADMIN")
-               .anyRequest().authenticated()
+               .anyRequest().permitAll()
                .and()
                .httpBasic();
     }
 
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth)
+            throws Exception
+    {
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password(passwordEncoder().encode("password2"))
+                .roles("ADMIN")
+                .and()
+                .withUser("user")
+                .password(passwordEncoder().encode("password2"))
+                .roles("USER");
+    }
 
-
-
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
 
 
